224abbd4cb9298dc7c821464206dc3e2812d78ab
abdhanan72 <abdhananvattoli@gmail.com>
2022-11-08 11:33:46 +0530

    changes

--- lib/drawer/drawer.dart
+++ lib/drawer/drawer.dart
@@ -5,29 +5,30 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Drawer(
-
+    return Drawer(
       child: ListView(
-        children: [const UserAccountsDrawerHeader(
-          decoration: BoxDecoration(color: Colors.black),
-          accountName:Text('ABD HANAN'), accountEmail: Text('hanan@unais.in'),
-        currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage('https://assets-prd.ignimgs.com/2022/09/15/cod-mw2-skull-1654191544255-1654722227152-1663251721478.jpeg'), 
-        ),
-        
-        ),
-        Column(
-          
-          children: const[
-            ListTile(
-
-            )
-            
-          ],
-        )
-
+        children: [
+          const UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Colors.black),
+            accountName: Text('ABD HANAN'),
+            accountEmail: Text('hanan@unais.in'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://assets-prd.ignimgs.com/2022/09/15/cod-mw2-skull-1654191544255-1654722227152-1663251721478.jpeg'),
+            ),
+          ),
+          Column(
+            children: [
+              ListTile(
+                tileColor: Colors.blueGrey,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                title: const Text('GO PREMIUM'),
+                leading: const Icon(Icons.workspace_premium),
+                onTap: () {},
+              )
+            ],
+          )
         ],
-
-      
       ),
     );
   }
